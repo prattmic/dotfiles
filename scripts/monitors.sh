2360f80b9e2a8ee6f3357cca7f4f360c942741fb
Michael Pratt <michael@pratt.im>
2015-04-15 20:42:03 -0700

    Disable LVDS1 if both outputs connected
    For some reason, xrandr can no longer get the other two outputs working
    if LVDS1 is still on.

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 # Configure monitors
-# If any are missing, they will not affect the others
 
-xrandr --output LVDS1 --preferred --left-of HDMI1
-xrandr --output HDMI1 --preferred --right-of LVDS1 --primary
-xrandr --output VGA1 --preferred --right-of HDMI1
+set -e
+
+function output_connected() {
+    local name=$1
+    xrandr | grep -q "$name connected"
+}
+
+# LVDS1 needs to be turned off before the others can be turned on
+if output_connected "HDMI1" && output_connected "VGA1"; then
+    xrandr --output LVDS1 --off
+fi
+
+
+xrandr --output HDMI1 --preferred --right-of LVDS1 --primary || true
+xrandr --output VGA1 --preferred --right-of HDMI1 || true
+xrandr --output LVDS1 --preferred --left-of HDMI1 || true
