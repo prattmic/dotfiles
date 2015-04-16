39743e96f6a597d59099e70c1cc7e5135283ac03
Michael Pratt <michael@pratt.im>
2015-04-15 20:25:03 -0700

    Move VGA1 right of HDMI1
    Make HDMI1 primary.

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -4,5 +4,5 @@
 # If any are missing, they will not affect the others
 
 xrandr --output LVDS1 --preferred --left-of HDMI1
-xrandr --output HDMI1 --preferred --right-of LVDS1
-xrandr --output VGA1 --preferred --left-of HDMI1 --primary
+xrandr --output HDMI1 --preferred --right-of LVDS1 --primary
+xrandr --output VGA1 --preferred --right-of HDMI1
