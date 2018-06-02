34e5f514192ebe3869ddf55e705391b66d6a0670
Michael Pratt <michael@pratt.im>
2018-06-02 11:33:18 -0700

    Add support for xmonad-log-applet

--- xmonad/xmonad-gnome.hs
+++ xmonad/xmonad-gnome.hs
@@ -1,6 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Hooks.DynamicLog
 
-main = xmonad gnomeConfig
-    { modMask = mod4Mask
+import qualified DBus as D
+import qualified DBus.Client as D
+import qualified Codec.Binary.UTF8.String as UTF8
+
+main :: IO ()
+main = do
+    dbus <- D.connectSession
+    getWellKnownName dbus
+    xmonad $ gnomeConfig
+         { modMask = mod4Mask
+         , logHook = dynamicLogWithPP (prettyPrinter dbus)
+         }
+
+prettyPrinter :: D.Client -> PP
+prettyPrinter dbus = defaultPP
+    { ppOutput   = dbusOutput dbus
+    , ppTitle    = pangoSanitize
+    , ppCurrent  = pangoColor "green" . wrap "[" "]" . pangoSanitize
+    , ppVisible  = pangoColor "yellow" . wrap "(" ")" . pangoSanitize
+    , ppHidden   = pangoColor "gray" . pangoSanitize
+    , ppUrgent   = pangoColor "red"
+    , ppLayout   = pangoSanitize
+    , ppSep      = " : "
     }
+
+getWellKnownName :: D.Client -> IO ()
+getWellKnownName dbus = do
+  D.requestName dbus (D.busName_ "org.xmonad.Log")
+                [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+  return ()
+
+dbusOutput :: D.Client -> String -> IO ()
+dbusOutput dbus str = do
+    let signal = (D.signal "/org/xmonad/Log" "org.xmonad.Log" "Update") {
+            D.signalBody = [D.toVariant ("<b>" ++ (UTF8.decodeString str) ++ "</b>")]
+        }
+    D.emit dbus signal
+
+pangoColor :: String -> String -> String
+pangoColor fg = wrap left right
+  where
+    left  = "<span foreground=\"" ++ fg ++ "\">"
+    right = "</span>"
+
+pangoSanitize :: String -> String
+pangoSanitize = foldr sanitize ""
+  where
+    sanitize '>'  xs = "&gt;" ++ xs
+    sanitize '<'  xs = "&lt;" ++ xs
+    sanitize '\"' xs = "&quot;" ++ xs
+    sanitize '&'  xs = "&amp;" ++ xs
+    sanitize x    xs = x:xs
