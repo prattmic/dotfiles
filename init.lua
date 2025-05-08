95e60703691b93d9cae93298ba0876d5837ea8c1
Michael Pratt <michael@pratt.im>
2025-05-07 23:18:36 -0400

    init.lua: basic nvim config

---
+++ init.lua
@@ -0,0 +1,56 @@
+-- Enable line numbers
+vim.opt.number = true
+
+-- Colors
+--
+-- The colors I want are actually the "light" background of "vim" with GUI
+-- colors disabled. This is despite having a dark background terminal.
+vim.cmd[[colorscheme vim]]
+vim.opt.bg = "light"
+vim.opt.termguicolors = false
+-- Fixup bad default highlight yellow.
+vim.cmd[[hi Search term=reverse ctermbg=3 guibg=Yellow]]
+-- Fixup bad default "Visual" (used for Telescope selection)
+vim.cmd[[hi Visual ctermfg=0 guifg=Black]]
+-- Fixup bad default bad spelling
+vim.cmd[[hi SpellBad ctermfg=0 guifg=Black]]
+-- Fixup bad default bad diff change
+vim.cmd[[hi DiffChange ctermfg=0 guifg=Black]]
+vim.cmd[[hi DiffText ctermfg=0 guifg=Black]]
+-- Fixup bad default diagnostic column background
+vim.cmd[[hi SignColumn ctermbg=none guibg=none]]
+-- Fixup bad default fold column background
+vim.cmd[[hi FoldColumn ctermbg=none guibg=none]]
+
+-- Always display sign column (diagnostics)
+vim.cmd[[set signcolumn=yes]]
+
+-- Display tabs
+vim.opt.list = true
+vim.opt.listchars = {
+	tab = '».'
+}
+
+-- Window switching
+vim.keymap.set('n', '<c-h>', '<c-w>h')
+vim.keymap.set('n', '<c-j>', '<c-w>j')
+vim.keymap.set('n', '<c-k>', '<c-w>k')
+vim.keymap.set('n', '<c-l>', '<c-w>l')
+
+-- Open newline without entering insert mode
+vim.keymap.set('n', 'go', 'o<esc>')
+vim.keymap.set('n', 'gO', 'O<esc>')
+
+-- Clear highlight on enter
+vim.keymap.set('n', '<cr>', ':noh<cr><cr>')
+
+-- Undofile
+vim.cmd[[set undofile]]
+vim.cmd[[au BufWritePre /tmp/* setlocal noundofile]]
+
+-- No mouse
+vim.cmd[[set mouse=]]
+
+-- Highlight trailing whitespace
+vim.cmd[[highlight ExtraWhitespace ctermbg=red guibg=red]]
+vim.cmd[[autocmd BufWinEnter * match ExtraWhitespace /\s\+$/]]
